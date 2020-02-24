dcca6c9098017d2ee64d8d89f4ee8f0990748445
Michael <mmathews@asas.qld.edu.au>
2020-02-24 14:00:56 +1000

    Updated sample code in activity 4_4

--- Activity04/Act04_proj4.py
+++ Activity04/Act04_proj4.py
@@ -5,29 +5,35 @@ Program Purpose : Area of Triangle Rectangle and square
 """
 
 # user defined function for area of Triangle
-def AreaTriangle(b,h):
-    triArea=0.5*b*h
-    print("The Triangle area is ",triArea," square units")
+
+
+def AreaTriangle(b, h):
+    triArea = 0.5*b*h
+    print("The Triangle area is ", triArea, " square units")
 
 # user defined function for area of Rectangle
-def AreaRectangle(b,h):
-    rectArea=b*h
-    print("The Rectangle area is ",rectArea," square units")
+
+
+def AreaRectangle(b, h):
+    rectArea = b*h
+    print("The Rectangle area is ", rectArea, " square units")
 
 # user defined function for area of Square
-def AreaSquare(b):
-    squArea=b*b
-    print("The square area is ",squArea," square units")
 
-# prompt User for Measurements 
-base=int(input("What is the base of the shape : "))
-height=int(input("What is the height of the shape : "))
+
+def AreaSquare(b):
+    squArea = b*b
+    print("The square area is ", squArea, " square units")
+
+
+# prompt User for Measurements
+base = int(input("What is the base of the shape : "))
+height = int(input("What is the height of the shape : "))
 
 # Out put Function Calculations
 print()
-AreaTriangle(base,height)
+AreaTriangle(base, height)
 print()
-AreaRectangle(base,height)
+AreaRectangle(base, height)
 print()
 AreaSquare(base)
-
